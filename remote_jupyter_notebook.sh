398fae7b35ce41354036f9ad4a2921f214fefb2a
eboigne <eboigne@stanford.edu>
2022-05-24 15:57:50 -0700

    First commit

---
+++ remote_jupyter_notebook.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# get tunneling info
+port=$(shuf -i8000-9999 -n1)
+node=$(hostname -s)
+user=$(whoami)
+cluster=$HOSTNAME # Check environment variables with `printenv` to look for cluster name
+
+# print tunneling instructions jupyter-log
+echo -e "
+In a separate local terminal, run:
+ssh -N -L ${port}:${node}:${port} ${user}@${cluster}
+"
+
+jupyter-notebook --no-browser --port=${port} --ip=${node}
